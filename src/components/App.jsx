34d8183a5c58cac13d225bbb8866e844304c30ec
VikaVelhan <velganvika@gmail.com>
2023-01-26 22:56:33 +0200

    localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -48,6 +48,21 @@ export class App extends Component {
     );
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('App componentDidUpdate ');
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { filter } = this.state;
 
